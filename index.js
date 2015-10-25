ad24dc3613f7e681dafd4eed84721a2416300ab3
Rudolf Manusachi <rud.manusachi@ru.ibm.com>
2015-10-25 16:20:16 +0300

    jnrfnbk

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ Cylon.robot({
     server: { adaptor: 'mqtt', host: 'mqtt://192.168.1.90:1883' }
   },
 
+  devices: {
+    socket1: { driver: 'mqtt', topic: 'socket1' },
+    socket2: { driver: 'mqtt', topic: 'socket2' },
+    socket3: { driver: 'mqtt', topic: 'socket3' },
+    socket4: { driver: 'mqtt', topic: 'socket4' }
+  },
+
   work: function(my) {
     var sockets = {
       "socket1": new mraa.Gpio(2),
@@ -13,27 +20,9 @@ Cylon.robot({
       "socket3": new mraa.Gpio(4),
       "socket4": new mraa.Gpio(5)
     };
-
-    var behaviour = {
-      "lordIsHere": function () {
-        var states = {};
-        for(socket in sockets){
-          states[socket] = sockets[socket].read();
-        }
-        my.connections.server.publish('socketsMyLord', JSON.stringify(states));
-      }      
-    }
-
+    
     for(socket in sockets){
-      behaviour[socket] = function (data) {
-        sockets[socket].dir(mraa.DIR_OUT);
-        sockets[socket].write(commands[data.toString()]);
-        my.connections.server.publish(socket+'/answer', 'success');
-      }
-    }
-
-    for(channel in behaviour){
-      my.connections.server.subscribe(channel);
+      my.connections.server.subscribe(socket);
     }
 
     var commands = {
@@ -43,7 +32,8 @@ Cylon.robot({
 
     my.connections.server.on('message', function (channel, data) {
       debugger;
-      behaviour[channel](data);
+      sockets[channel].dir(mraa.DIR_OUT);
+      sockets[channel].write(commands[data.toString()]);
     });
   }
 }).start();
