a3ce38694d08556447274f061b627d12f3c22e48
Rudolf Manusachi <rud.manusachi@ru.ibm.com>
2015-10-25 07:28:39 +0300

    added debugger

--- index.js
+++ index.js
@@ -31,13 +31,9 @@ Cylon.robot({
     };
 
     my.connections.server.on('message', function (channel, data) {
-      console.log(channel, commands[data.toString])
+      debugger;
       sockets[channel].dir(mraa.DIR_OUT);
       sockets[channel].write(commands[data.toString()]);
-    });
-
-    every((5).seconds(), function() {
-      my.connections.server.publish('socket1', 'on');
     });
   }
 }).start();
